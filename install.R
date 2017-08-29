c2bea241df5cebf15ce003d012d9df433646f233
bbimber <bbimber@gmail.com>
2017-08-29 10:48:16 -0700

    install all through bioconductor

--- install.R
+++ install.R
@@ -1,14 +1,7 @@
-list.of.packages <- c('digest', 'plyr', 'perm', 'reshape', 'reshape2', 'knitr', 'ggplot2', 'Rcpp', 'lattice', 'tidyverse', 'stringr', 'edgeR', 'gplots', 'pheatmap', 'Rtsne', 'Rlabkey', 'devtools')
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,'Package'])]
-if(length(new.packages)){
-	install.packages(new.packages, dependencies=TRUE, repos='http://cran.rstudio.com')
-} else {
-	print('no updates needed')
-}
+list.of.packages <- c('digest', 'plyr', 'perm', 'reshape', 'reshape2', 'knitr', 'ggplot2', 'Rcpp', 'lattice', 'tidyverse', 'stringr', 'gplots', 'pheatmap', 'Rtsne', 'Rlabkey', 'devtools', 'codetools', 'RcppEigen', 'Cairo', 'rgl', 'limma', 'fry', 'goana', 'kegga', 'plotMD', 'plotWithHighlights', 'DESeq2', 'edgeR', 'glmQLFit', 'statmod', 'biomaRt', 'sva', 'scater', 'scran', 'destiny', 'WGCNA')
 
 source('http://bioconductor.org/biocLite.R')
-biocLite(c('RcppEigen', 'Cairo', 'rgl', 'limma', 'fry', 'goana', 'kegga', 'plotMD', 'plotWithHighlights', 'DESeq2', 'edgeR', 'glmQLFit', 'statmod', 'biomaRt', 'sva', 'scater', 'scran', 'destiny', 'WGCNA', ask=FALSE, dependencies=TRUE)
-#biocLite('scde', ask=FALSE, dependencies=TRUE)
+biocLite(list.of.packages, ask=FALSE, dependencies=TRUE)
 
 # see: 
 # https://github.com/hms-dbmi/scde/issues/48
