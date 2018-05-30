20b7e055f738c4c0898438bdbece60c7dc9d415e
bbimber <bbimber@gmail.com>
2018-05-29 17:37:24 -0700

    no need to install rcurl

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-list.of.packages <- c('digest', 'plyr', 'dtplyr', 'perm', 'reshape', 'reshape2', 'knitr', 'ggplot2', 'Rcpp', 'lattice', 'tidyverse', 'stringr', 'gplots', 'pheatmap', 'Rtsne', 'Rlabkey', 'devtools', 'codetools', 'RcppEigen', 'Cairo', 'rgl', 'limma', 'fry', 'goana', 'kegga', 'plotMD', 'plotWithHighlights', 'DESeq2', 'edgeR', 'glmQLFit', 'statmod', 'biomaRt', 'sva', 'Matrix', 'WGCNA', 'RCurl')
+list.of.packages <- c('digest', 'plyr', 'dtplyr', 'perm', 'reshape', 'reshape2', 'knitr', 'ggplot2', 'Rcpp', 'lattice', 'tidyverse', 'stringr', 'gplots', 'pheatmap', 'Rtsne', 'Rlabkey', 'devtools', 'codetools', 'RcppEigen', 'Cairo', 'rgl', 'limma', 'fry', 'goana', 'kegga', 'plotMD', 'plotWithHighlights', 'DESeq2', 'edgeR', 'glmQLFit', 'statmod', 'biomaRt', 'sva', 'Matrix', 'WGCNA')
 
 source('http://bioconductor.org/biocLite.R')
 biocLite(list.of.packages, ask=FALSE, dependencies=TRUE)
