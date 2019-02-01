11cdc44adee92584929aa1a1ee63aee9b04ab7f7
bbimber <bbimber@gmail.com>
2019-01-31 16:37:26 -0800

    bugfix seurat docker

--- seurat/install.R
+++ seurat/install.R
@@ -1,2 +1,10 @@
 install.packages('devtools',dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+if (!requireNamespace("BiocManager"))
+    install.packages("BiocManager")
+BiocManager::install()
+
+toInstall <- c('DESeq2', 'destiny', 'MAST', 'S4Vectors', 'SingleCellExperiment', 'SummarizedExperiment', 'dplyr', 'dtplyr', 'enrichR')
+BiocManager::install(toInstall, update = TRUE, ask = FALSE)
+
 devtools::install_github(repo = 'satijalab/seurat', ref = 'release/3.0', dependencies = T, upgrade = 'always')
