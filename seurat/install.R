1a7740e59b2f7aecb9abe9c72bd59571cc76e688
bbimber <bbimber@gmail.com>
2019-03-11 12:27:27 -0700

    Switch to bioconductor base for docker

--- seurat/install.R
+++ seurat/install.R
@@ -4,7 +4,9 @@ if (!requireNamespace("BiocManager"))
     install.packages("BiocManager")
 BiocManager::install()
 
-toInstall <- c('DESeq2', 'destiny', 'MAST', 'S4Vectors', 'SingleCellExperiment', 'SummarizedExperiment', 'dplyr', 'dtplyr', 'enrichR')
-BiocManager::install(toInstall, update = TRUE, ask = FALSE)
+toInstall <- c('DESeq2', 'destiny', 'MAST', 'S4Vectors', 'SingleCellExperiment', 'SummarizedExperiment', 'dplyr', 'dtplyr', 'enrichR', 'rtracklayer')
+BiocManager::install(toInstall, update = FALSE, ask = FALSE)
+
+suppressWarnings(BiocManager::install(update=TRUE, ask=FALSE))
 
 devtools::install_github(repo = 'satijalab/seurat', ref = 'release/3.0', dependencies = T, upgrade = 'always')
