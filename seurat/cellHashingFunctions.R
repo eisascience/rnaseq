327f9f7f4344ff1f237676e4e0e1e8731f74131a
bbimber <bbimber@gmail.com>
2019-03-14 10:01:13 -0700

    Add multiseq packages to docker

--- seurat/cellHashingFunctions.R
+++ seurat/cellHashingFunctions.R
@@ -161,6 +161,90 @@ doHtoDemux <- function(seuratObj) {
   return(seuratObj)
 }
 
-generateCellHashCallsMultiSeq <- function(barcodeData) {
+generateCellHashCallsMultiSeq <- function(barcodeData, outputPdf = 'multiseq.bc.check.pdf') {
+  #transpose CITE-seq count input:
+  bar.table <- data.frame(t(barcodeData))
   
+  bar.tsne <- barTSNE(bar.table[,1:96]) 
+  
+  #pdf(outputPdf)
+  for (i in 3:ncol(bar.tsne)) {
+    g <- ggplot(bar.tsne, aes(x = TSNE1, y = TSNE2, color = bar.tsne[,i])) +
+      geom_point() +
+      scale_color_gradient(low = "black", high = "red") +
+      ggtitle(colnames(bar.tsne)[i]) +
+      theme(legend.position = "none") 
+    print(g)
+  }
+  #dev.off()
+  
+  ## Round 1 -----------------------------------------------------------------------------------------------------
+  ## Perform Quantile Sweep
+  bar.table.full <- bar.table[,1:96]
+  bar.table <- bar.table.full[, good.bars]  # Remove missing bars and summary columns
+  bar.table_sweep.list <- list()
+  n <- 0
+  for (q in seq(0.01, 0.99, by=0.02)) {
+    print(q)
+    n <- n + 1
+    bar.table_sweep.list[[n]] <- classifyCells(bar.table, q=q)
+    names(bar.table_sweep.list)[n] <- paste("q=",q,sep="")
+  }
+  
+  ## Identify ideal inter-maxima quantile to set barcode-specific thresholds
+  findThresh(call.list=bar.table_sweep.list, id="round1")
+  print(paste0('Round 1: ', extrema_round1))
+  ggplot(data=res_round1, aes(x=q, y=Proportion, color=Subset)) + 
+    geom_line() + 
+    theme(legend.position = "none") +
+    geom_vline(xintercept=extrema_round1, lty=2) + 
+    scale_color_manual(values=c("red","black","blue")
+   )
+  
+  ## Finalize round 1 classifications, remove negative cells
+  round1.calls <- classifyCells(bar.table, q=findQ(res_round1, extrema_round1))
+  neg.cells <- names(round1.calls)[which(round1.calls == "Negative")]
+  bar.table <- bar.table[-which(rownames(bar.table) %in% neg.cells), ]
+  
+  ## Round 2 -----------------------------------------------------------------------------------------------------
+  bar.table_sweep.list <- list()
+  n <- 0
+  for (q in seq(0.01, 0.99, by=0.02)) {
+    print(q)
+    n <- n + 1
+    bar.table_sweep.list[[n]] <- classifyCells(bar.table, q=q)
+    names(bar.table_sweep.list)[n] <- paste("q=",q,sep="")
+  }
+  
+  findThresh(call.list=bar.table_sweep.list, id="round2")
+  print(paste0('Round 1: ', extrema_round2))
+  round2.calls <- classifyCells(bar.table, q=findQ(res_round2, extrema_round2))
+  neg.cells <- c(neg.cells, names(round2.calls)[which(round2.calls == "Negative")])
+  
+  ## Repeat until all no negative cells remain (usually 3 rounds)...
+  final.calls <- c(round2.calls, rep("Negative",length(neg.cells)))
+  names(final.calls) <- c(names(round2.calls),neg.cells)
+  
+  
+  return(final.calls)
+}
+
+reclassifyByMultiSeq <- function(bar.table, final.calls){
+  reclass.cells <- findReclassCells(bar.table.full, names(final.calls)[which(final.calls=="Negative")])
+  reclass.res <- rescueCells(bar.table.full, final.calls, reclass.cells)
+  
+  ## Visualize Results
+  print(ggplot(reclass.res[-1, ], aes(x=ClassStability, y=MatchRate_mean)) + 
+    geom_point() + xlim(c(nrow(pool.reclass.res)-1,1)) + 
+    ylim(c(0,1.05)) +
+    geom_errorbar(aes(ymin=MatchRate_mean-MatchRate_sd, ymax=MatchRate_mean+MatchRate_sd), width=.1) +
+    geom_hline(yintercept = reclass.res$MatchRate_mean[1], color="red") +
+    geom_hline(yintercept = reclass.res$MatchRate_mean[1]+3*reclass.res$MatchRate_sd[1], color="red",lty=2) +
+    geom_hline(yintercept = reclass.res$MatchRate_mean[1]-3*reclass.res$MatchRate_sd[1], color="red",lty=2)
+  )
+  
+  ## Finalize negative cell rescue results
+  final.calls.rescued <- final.calls
+  rescue.ind <- which(reclass.cells$ClassStability >= 16) ## Note: Value will be dataset-specific
+  final.calls.rescued[rownames(reclass.cells)[rescue.ind]] <- reclass.cells$Reclassification[rescue.ind]
 }
--- seurat/install.R
+++ seurat/install.R
@@ -4,9 +4,11 @@ if (!requireNamespace("BiocManager"))
     install.packages("BiocManager")
 BiocManager::install()
 
-toInstall <- c('DESeq2', 'destiny', 'MAST', 'S4Vectors', 'SingleCellExperiment', 'SummarizedExperiment', 'dplyr', 'dtplyr', 'enrichR', 'rtracklayer', 'data.table', 'naturalsort', 'Rlabkey')
+toInstall <- c('DESeq2', 'destiny', 'MAST', 'S4Vectors', 'SingleCellExperiment', 'SummarizedExperiment', 'dplyr', 'dtplyr', 'enrichR', 'data.table', 'naturalsort', 'Rlabkey', 'KernSmooth', 'reshape2', 'Rtsne')
 BiocManager::install(toInstall, update = FALSE, ask = FALSE)
 
 suppressWarnings(BiocManager::install(update=TRUE, ask=FALSE))
 
 devtools::install_github(repo = 'satijalab/seurat', ref = 'release/3.0', dependencies = T, upgrade = 'always')
+
+devtools::install_github('chris-mcginnis-ucsf/MULTI-seq')
